9d63a82bd9448ad3723c33769764b3ef51cbe3f5
noriokun4649 <noriokun4649@gmail.com>
2019-05-22 01:35:31 +0900

    いらんusingさくじぃお

--- TSManager/MainWindow.xaml.cs
+++ TSManager/MainWindow.xaml.cs
@@ -1,21 +1,11 @@
-﻿using MaterialDesignThemes.Wpf;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TSManager
 {
