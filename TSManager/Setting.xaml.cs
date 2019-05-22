736df37ed8a70b603018b721b63357bf9ebd2a6a
noriokun4649 <noriokun4649@gmail.com>
2019-05-23 05:04:04 +0900

    設定画面の細かい修正

--- TSManager/Setting.xaml.cs
+++ TSManager/Setting.xaml.cs
@@ -1,18 +1,5 @@
-﻿using Microsoft.Win32;
-using Microsoft.WindowsAPICodePack.Dialogs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace TSManager
 {
