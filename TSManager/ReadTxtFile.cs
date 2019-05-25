80e26d4e986b02dc7c51484454f9904b0e93d75e
noriokun4649 <noriokun4649@outlook.com>
2019-05-25 22:30:20 +0900

    TS内からも番組情報を取得できるように。追加

--- TSManager/ReadTxtFile.cs
+++ TSManager/ReadTxtFile.cs
@@ -3,32 +3,98 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace TSManager
 {
     class ReadTxtFile
     {
-        private DateTime starttime;
-        private DateTime endtime;
-        private TimeSpan length;
-        private string company;
-        private string title;
-        private string subtitle;
-        private string series;
-        private int epinum;
-        private List<string> seriesInfo = new List<string>();
-        private List<int> genreIndex = new List<int>();
-        private List<string> genre = new List<string>();
-
+        string flag = @"\[新\]|\[終\]|\[再\]|\[字\]|\[デ\]|\[解\]|\[無\]|\[二\]|\[S\]|\[SS\]|\[初\]|\[生\]|\[Ｎ\]|\[映\]|\[多\]|\[双\]";
+        string subtitleflag = @"「.+」|【.+】";
+        string tvIndex = @"[#＃♯](?<index>[0-9０-９]{1,3})|[第](?<index>[0-9０-９]{1,3})[話回]";
 
         public ReadTxtFile(string filepath)
         {
-            var flag = @"\[新\]|\[終\]|\[再\]|\[字\]|\[デ\]|\[解\]|\[無\]|\[二\]|\[S\]|\[SS\]|\[初\]|\[生\]|\[Ｎ\]|\[映\]|\[多\]|\[双\]";
-            var subtitleflag = @"「.+」|【.+】";
-            var tvIndex = @"[#＃♯](?<index>[0-9０-９]{1,3})|[第](?<index>[0-9０-９]{1,3})[話回]";
+            if (!Properties.Settings.Default.Mode)
+            {
+                try
+                {
+                    GetFromTxt(filepath);
+                }
+                catch
+                {
+                    GetFromEit(filepath);
+                }
+            }
+            else
+            {
+                GetFromEit(filepath);
+            }
+        }
+
+        private void GetFromEit(string filepath)
+        {
+            using (var process = new Process())
+            {
+                try
+                {
+                    process.StartInfo = new ProcessStartInfo
+                    {
+                        FileName = Util.file,
+                        Arguments = $"\"{filepath.Replace(".program.txt", "")}\" -cbidtpg -l 10",
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                    };
+                    process.Start();
+                    var infos = process.StandardOutput.ReadToEnd().Split(',');
+                    Company = infos[0];
+                    Title = infos[1];
+                    Subtitle = infos[2];
+                    var day = infos[3];
+                    var starttime = infos[4];
+                    var genres = infos[6].Split('　');
+                    Length = TimeSpan.Parse(infos[5]);
+                    Starttime = DateTime.Parse(day + " " + starttime);
+                    Endtime = Starttime.Add(Length);
+                    Series = Regex.Replace(Title, flag + "|" + subtitleflag + "|" + tvIndex, "");
+                    if (Series.Split('　').Length > 1)
+                    {
+                        Series = Series.Split('　')[0];
+                    }
+                    var matche = Regex.Matches(Title, subtitleflag);
+                    foreach (Match ma in matche)
+                    {
+                        SeriesInfo.Add(ma.Value);
+                    }
+                    foreach (var genre in genres)
+                    {
+                        GenreIndex.Add(GenreIndexRet(genre));
+                        Genre.Add(genre.Split(' ')[0]);
+                    }
+                    if (Regex.IsMatch(Title, tvIndex))
+                    {
+                        var tvidex_mat = Regex.Match(Title, tvIndex);
+                        Epinum = int.Parse(tvidex_mat.Groups["index"].Value.Normalize(NormalizationForm.FormKC));
+                    }
+                    else
+                    {
+                        Epinum = 0;
+                    }
+                }
+                catch
+                {
+                    throw new AggregateException();
+                }
+            }
+        }
+
+        private void GetFromTxt(string filepath)
+        {
             var file = File.ReadAllLines(filepath);
             var time = file[0];
             Company = file[1];
@@ -37,10 +103,8 @@ namespace TSManager
             var timesprit = time.Split(' ');
             var day = Regex.Replace(timesprit[0], @"\([月|火|水|木|金|土|日]\)", "");
             var time_sprit = timesprit[1].Split('～');
-            
-            
 
-            Starttime = DateTime.Parse(day+" "+time_sprit[0]);
+            Starttime = DateTime.Parse(day + " " + time_sprit[0]);
             Endtime = DateTime.Parse(day + " " + time_sprit[1]);
 
             if (Endtime.CompareTo(Starttime) == -1)
@@ -66,11 +130,11 @@ namespace TSManager
                 Subtitle += "\n" + file[i];
                 i++;
             }
-            var genreIndexCount = Array.IndexOf(file, "ジャンル : ");
+            var genreIndexCount = Array.IndexOf(file, "ジャンル : ") + 1;
             while (file[genreIndexCount] != "")
             {
-                genreIndex.Add(GenreIndexRet(file[genreIndexCount]));
-                genre.Add(file[genreIndexCount].Split(' ')[0]);
+                GenreIndex.Add(GenreIndexRet(file[genreIndexCount]));
+                Genre.Add(file[genreIndexCount].Split(' ')[0]);
                 genreIndexCount++;
             }
             if (Regex.IsMatch(Title, tvIndex))
@@ -95,16 +159,16 @@ namespace TSManager
             return Array.IndexOf(Util.genres, r);
         }
 
-        public string Company { get => company; set => company = value; }
-        public string Title { get => title; set => title = value; }
-        public string Subtitle { get => subtitle; set => subtitle = value; }
-        public List<string> Genre { get => genre; set => genre = value; }
-        public List<int> GenreIndex { get => genreIndex; set => genreIndex = value; }
-        public string Series { get => series; set => series = value; }
-        public List<string> SeriesInfo { get => seriesInfo; set => seriesInfo = value; }
-        public int Epinum { get => epinum; set => epinum = value; }
-        public DateTime Starttime { get => starttime; set => starttime = value; }
-        public DateTime Endtime { get => endtime; set => endtime = value; }
-        public TimeSpan Length { get => length; set => length = value; }
+        public string Company { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public List<string> Genre { get; set; } = new List<string>();
+        public List<int> GenreIndex { get; set; } = new List<int>();
+        public string Series { get; set; }
+        public List<string> SeriesInfo { get; set; } = new List<string>();
+        public int Epinum { get; set; }
+        public DateTime Starttime { get; set; }
+        public DateTime Endtime { get; set; }
+        public TimeSpan Length { get; set; }
     }
 }
