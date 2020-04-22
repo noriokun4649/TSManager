bb585f86d97105421921b663eef2cfd54374a363
noriokun4649 <noriokun4649@outlook.com>
2020-04-23 06:35:39 +0900

    再生・情報ボタンに機能割り当て

--- TSManager/UserControlListItem.xaml.cs
+++ TSManager/UserControlListItem.xaml.cs
@@ -24,5 +24,30 @@ namespace TSManager
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Files file = (Files)DataContext;
+            Util.OpenFile(file.FilePath, file.FileName);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Files file = (Files)DataContext;
+            var wasu = file.Epinum == 0 ? "なし" : "第" + file.Epinum.ToString() + "話";
+            var subtitle = file.TvSeriesInfo.LongCount() == 0 ? "なし" : file.TvSeriesInfo[0];
+            MessageBox.Show(
+            "ファイル名:" + file.FileName + "\n" +
+            "ファイルパス:" + file.FilePath + "\n" +
+            "サブタイトル1:" + subtitle + "\n" +
+            "シリーズ名:" + file.TvSeries + "\n" +
+            "話数:" + wasu + "\n" +
+            "ジャンル1:" + file.Genres[0] + "\n" +
+            "放送局:" + file.Company + "\n" +
+            "放送開始時間:" + file.StartTime.ToString("yyyy年MM月dd日(dddd) tthh時mm分") + "\n" +
+            "放送終了時間:" + file.EndTime.ToString("yyyy年MM月dd日(dddd) tthh時mm分") + "\n" +
+            "放送時間:" + file.Length.ToString(@"hh'時間'mm'分'ss'秒'") + "\n"
+            );
+        }
     }
 }
