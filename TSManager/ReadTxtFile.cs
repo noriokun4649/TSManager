26f669ac65fe93d384959f7673ea808aafb48f93
noriokun4649 <noriokun4649@outlook.com>
2020-10-06 16:41:08 +0900

    リファクタリング

--- TSManager/ReadTxtFile.cs
+++ TSManager/ReadTxtFile.cs
@@ -2,12 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace TSManager
 {
